a8b7f15c3e62d5a9c5a60b26bf4a6351cff60b11
dhiraj giri <2021356392.giri@ug.sharda.ac.in>
2023-09-16 03:32:22 +0530

    'footer'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a63bc91931c98e2c"; }
+/******/ 		__webpack_require__.h = function() { return "e48407cd5984f4b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
